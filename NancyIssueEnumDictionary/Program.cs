b57ec41e5f6ae4c8054adc9de81823dabee07d0b
CallumVass <callum.vass@gmail.com>
2014-10-01 13:09:45 +0100

    Initial

---
+++ NancyIssueEnumDictionary/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace NancyIssueEnumDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:1235")))
+            {
+                host.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            // wont work
+            Get["/test"] = _ =>
+            {
+                var data = new ResponseObject {Name = "Test"};
+
+                data.Dictionary.Add(MyEnum.Test, "Test");
+
+                return data;
+            };
+
+            // will work
+            Get["/test2"] = _ =>
+            {
+                var data = new ResponseObject {Name = "Test 2"};
+                return data;
+            };
+        }
+    }
+
+    public class ResponseObject
+    {
+        public string Name { get; set; }
+        public Dictionary<MyEnum, string> Dictionary { get; set; }
+
+        public ResponseObject()
+        {
+            Dictionary = new Dictionary<MyEnum, string>();
+        }
+    }
+
+    public enum MyEnum
+    {
+        Test,
+        TestTwo
+    }
+}
